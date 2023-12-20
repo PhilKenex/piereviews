c0b69f0a0088ffa29a5c0825547113a4de309c89
Phil Greville <phil@kenex.co.nz>
2023-12-20 21:50:40 +1300

    updated website with 4 pages

---
+++ app.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    function animateCounter(id, finalNumber, duration) {
+        const stepTime = 50; // Time between each step in milliseconds
+        const steps = duration / stepTime; // Total number of steps
+        const increment = finalNumber / steps; // Increment per step
+        let currentNumber = 0; // Starting number
+
+        const counterElement = document.getElementById(id);
+        const interval = setInterval(() => {
+            currentNumber += increment;
+            counterElement.textContent = Math.min(Math.round(currentNumber), finalNumber); // Update the number in the HTML
+
+            if (currentNumber >= finalNumber) {
+                clearInterval(interval); // Stop the interval when the final number is reached
+            }
+        }, stepTime);
+    }
+
+    // Animate each counter
+    animateCounter('piesCounter', 207, 2000); // Pies
+    animateCounter('shopsCounter', 96, 2000); // Pie Shops
+    animateCounter('flavoursCounter', 48, 2000); // Flavours
+};
