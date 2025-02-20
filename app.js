353f8943893f7f603d55eb678d329b8e6350531b
Phil Greville <phillip.greville89@gmail.com>
2025-02-21 09:38:46 +1300

    update 20250221

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ window.onload = function() {
     }
 
     // Animate each counter
-    animateCounter('piesCounter', 612, 2000); // Pies
-    animateCounter('shopsCounter', 218, 2000); // Pie Shops
+    animateCounter('piesCounter', 613, 2000); // Pies
+    animateCounter('shopsCounter', 219, 2000); // Pie Shops
     animateCounter('flavoursCounter', 114, 2000); // Flavours
 };
 
